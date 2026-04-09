3be13cc3c88e33fe83d6835f65d1069f1a5152e4
wh01s17 <jor.perezo@hotmail.com>
2026-04-08 20:11:54 -0400

    add: class 02 lesson 02 module 04 0041-2

---
+++ RTD-24-01-02-0041-2/modulo-04/leccion-02/01-ejemplo/assets/js/main.js
@@ -0,0 +1,13 @@
+let numero = 1;
+
+if (confirm("Probar x++?")) {
+    console.log("Resultado de x++: ", numero++);
+    console.log("Valor despues de x++: ", numero);
+} else {
+    console.log("Cancelaste la prueba");
+}
+
+// let numero2 = 1;
+
+// console.log("Resultado de ++x: ", ++numero2);
+// console.log("Valor despues de ++x: ", numero2);
